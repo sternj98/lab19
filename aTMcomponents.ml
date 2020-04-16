c875a0ffd47f5cfe28e44468f9dacf7258afd55a
J. Patrick Connor <connorpa@J-Patricks-MacBook-Pro.local>
2020-04-16 13:09:32 -0400

    updated

--- aTMcomponents.ml
+++ aTMcomponents.ml
@@ -24,7 +24,7 @@ class atm (accounts : account_spec list) =
   object(this)
 
   val mutable db = []
-  
+
   (* initialize accts -- Establishes a database of accounts, each with a
      name, aribtrary id, and balance. The names and balances are
      initialized as per the `accts` provided. *)
@@ -61,18 +61,33 @@ class atm (accounts : account_spec list) =
 
   (* get_balance id -- Returns the balance for the customer account with
      the given id. *)
-  method get_balance : int =
-    failwith "TODO";;
+  method get_balance (n : id) : int =
+    let iter num lst : int =
+      match lst with
+      | [] -> exception "Not_found"
+      | hd :: tl -> if hd.id = num then hd.balance
+                    else iter num tl in
+    iter n db ;;
 
   (* get_name id -- Returns the name associated with the customer
      account with the given id. *)
-  method get_name : string =
-    failwith "TODO";;
+  method get_name (n : id) : string =
+    let iter num lst : int =
+      match lst with
+      | [] -> exception "Not_found"
+      | hd :: tl -> if hd.id = num then hd.name
+                    else iter num tl in
+    iter n db ;;
 
   (* update_balance id amount -- Modifies the balance of the customer
      account with the given id,setting it to the given amount. *)
-  method update_balance : id -> int -> unit  =
-  ;;
+  method update_balance (n : id) (new_bal : int): unit  =
+    let iter id_val num lst : int =
+      match lst with
+      | [] -> exception "Not_found"
+      | hd :: tl -> if hd.id = id_val then hd.bal <- num
+                    else iter num tl in
+    iter n new_bal db ;;
 
   (*....................................................................
     Presenting information and cash to the customer
@@ -80,10 +95,16 @@ class atm (accounts : account_spec list) =
 
   (* present_message message -- Presents to the customer (on stdout) the
      given message followed by a newline. *)
-  method present_message : string -> unit ;;
+  method present_message (s: string) = 
+    print_endline s   ;;
 
-  (* deliver_cash amount -- Dispenses the given amount of cash to the
-     customer (really just prints to stdout a message to that
-     effect). *)
-  method deliver_cash : int -> unit ;;
-end
+  (* deliver_cash amount -- Dispenses the given amount of cash to the 
+  customer (really just prints to stdout a message to that 
+  effect). *) 
+
+  method deliver_cash (n : int) = 
+    while n >= 20 do 
+      Printf.printf "[20 @ 20]"; 
+      n = n - 20 
+    done 
+    print_endline (sprintf " and %d more" n) ;;
